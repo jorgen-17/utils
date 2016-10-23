c4cd6c35d04dc29645a5d14239c795763848436d
Jorge Nonell <Jorge Nonell>
2016-10-23 11:20:40 -0400

    adding git aliases shell script and standard git ignore for visual studio projects

---
+++ git/gitAliases.sh
@@ -0,0 +1,7 @@
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.cam 'commit -am'
+git config --global alias.st status
+git config --global alias.aa 'add --all'
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
